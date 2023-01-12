b937622c6a5e9085aadc81da06f5136c55a9db29
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-12 19:24:50 +0300

    updated fully functioning[[200~24372e1]

--- pages/index.vue
+++ pages/index.vue
@@ -43,10 +43,7 @@
     <label for="my-modal-4" class="modal cursor-pointer">
       <label class="modal-box relative" for="">
         <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
-        <p class="py-4">
-          You've been selected for a chance to get one year of subscription to use
-          Wikipedia for free!
-        </p>
+        <p class="py-4"></p>
       </label>
     </label>
     <div
